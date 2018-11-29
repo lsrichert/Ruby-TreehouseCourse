50d171cbe761261d4ebb702b486dbe8733219a17
Lauren Richert <lsrichert@gmail.com>
2018-11-29 16:07:20 -0600

    working with methods

---
+++ parameters.rb
@@ -0,0 +1,11 @@
+def add(first, second)
+puts first, second
+puts first + second
+end
+
+def subtract(first, second)
+    puts first, second
+    puts first - second
+end
+
+add(100, 50)
---
+++ variables.rb
@@ -0,0 +1,23 @@
+greeting = "hello"
+number = 10
+puts number
+puts number + 2
+puts greeting
+puts number
+puts 12 - number
+puts number + 2
+#you can change the value of a variable anywhere; from that point on, the variable will hold a different value than before
+number = 4
+puts number
+#Valid variable names - same as method names.
+#All lower case
+#Numbers are legal but rarely used
+#Snake case - separate words with underscores. 
+
+#Challenge
+##Assign the string "Ruby" to a variable named language. Then assign the string "awesome" to a variable named description. 
+#Then make a single call to puts and pass it two arguments: the language and description variables. (Remember that if you're providing multiple arguments for a method call, you separate them with commas.)
+
+language = 'Ruby'
+description = 'awesome'
+puts language, description
