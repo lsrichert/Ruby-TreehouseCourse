cd859e630486de5fed657bc7873ae6e5b7abf3d8
Lauren Richert <lsrichert@gmail.com>
2018-12-12 14:26:25 -0600

    completed extra credit from basics portion of course

--- extra.rb
+++ extra.rb
@@ -3,3 +3,15 @@
 Write a method that accepts the length, width, and height of a box, and returns its volume (length * width * height).
 Write a method that accepts the radius of a circle, and returns its area (Math::PI * radius ** 2).
 /
+def box(length, width, height)
+    length * width * height 
+end
+
+puts box(5, 4, 2)
+
+
+def circle(radius)
+    radius * 2
+end
+
+puts circle(10)
