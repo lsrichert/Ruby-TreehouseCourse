5ed9996294e14d724ec2dc147f2ddf0a98e4d0b1
Lauren Richert <lsrichert@gmail.com>
2018-12-18 12:15:51 -0600

    started string concatenation and did challenge on treehouse

--- widgets.rb
+++ widgets.rb
@@ -1,6 +1,6 @@
 
 def ask(question)
-    print question
+    print question + " "
     answer = gets
     puts answer
 end
@@ -8,5 +8,20 @@ end
 puts "Welcome to the widget store!"
 ask("How many widgets are you ordering?")
 
+
 #calling the puts method sends the cursor to the next line; calling the print method keeps the cursor
 #at the end of the current line at the end of the call
+
+def ask(question)
+    print question + " "
+    gets
+end
+
+puts "Welcome to the new widget store!"
+    answer = ask("How many new widgets are you ordering?")
+    puts "You entered" + " " + answer + "widgets."
+
+    #Challenge-
+    #Concatenate the strings "Ruby" and "rocks" and then print using puts method.
+    var = "Ruby " + "rocks"
+    puts var
