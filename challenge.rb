abc6393a28f50b570f6a89b7e34aaa141ce339dd
Lauren Richert <lsrichert@gmail.com>
2018-12-18 12:58:58 -0600

    escape sequences

---
+++ challenge.rb
@@ -0,0 +1,17 @@
+#Call puts with a string containing a newline character. Then call puts with a string containing 
+#a tab character. Finally, call puts a third time with a string containing a " double quotation mark.
+
+puts "hello A\n hello"
+puts "hello B\t hello"
+puts "hello C \"hello\""
+
+#Here's some code that uses escape sequences:
+
+puts "first line\nsecond line"
+puts "\tindented text"
+puts "He said, \"Whoa.\""
+#Output:
+#first line
+#second line
+#   indented text
+#He said, "Whoa."
--- widgets.rb
+++ widgets.rb
@@ -19,9 +19,15 @@ end
 
 puts "Welcome to the new widget store!"
     answer = ask("How many new widgets are you ordering?")
-    puts "You entered" + " " + answer + "widgets."
+    puts "You entered" #{answer} "widgets."
 
     #Challenge-
     #Concatenate the strings "Ruby" and "rocks" and then print using puts method.
     var = "Ruby " + "rocks"
     puts var
+
+    puts "first line\nsecond line"
+    puts "\tindented text"
+    puts "He said, \"Whoa.\""
+    
+
