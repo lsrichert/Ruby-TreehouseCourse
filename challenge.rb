291525e83f8290dd286fea1b76f5d3700e9e3b83
Lauren Richert <lsrichert@gmail.com>
2018-12-18 13:18:37 -0600

    completed practice session for strings and escape sequences

--- challenge.rb
+++ challenge.rb
@@ -15,3 +15,22 @@ puts "He said, \"Whoa.\""
 #second line
 #   indented text
 #He said, "Whoa."
+
+#Practice on strings and escape sequences:
+
+# This program should give users a brief tutorial on using escape sequences in Ruby.
+# Write some code that prints the following (without comment markers) when run:
+# 
+# When you include \t in a double-quoted string in Ruby, the output looks like this:
+# before  after
+# When you include \n in a double-quoted string, the output looks like this:
+# before
+# after
+# 
+# The first "before"/"after" pair should have a single tab character between them,
+# NOT space characters.
+
+puts 'When you include \t in a double-quoted string in Ruby, it looks like this:'
+puts "before\tafter"
+puts 'When you include \n in a double-quoted string in Ruby, it looks like this:'
+puts "before\nafter"
